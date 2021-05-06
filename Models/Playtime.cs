170fbc841867fe88edfde3a074e53bf7a58ab5eb
Victoria Tiller <vijotiller@gmail.com>
2021-05-06 15:57:10 -0400

    whyyy

--- Models/Playtime.cs
+++ Models/Playtime.cs
@@ -5,7 +5,7 @@ namespace TamagotchiAPI.Models
     public class Playtime
     {
         public int Id { get; set; }
-        public DateTime When { get; set; }
+        public DateTime When { get; set; } = DateTime.Now;
         public int PetId { get; set; }
         public Pet Pet { get; set; }
     }
